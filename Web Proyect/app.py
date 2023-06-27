f295280e4070292959255dec0f29272474895c22
martinvegabollmann <martinvegabollmann>
2023-06-27 05:51:11 +0100

    Second Update

--- Web Proyect/app.py
+++ Web Proyect/app.py
@@ -7,5 +7,21 @@ app=Flask(__name__)
 def start():
     return render_template('site/index.html')
 
+@app.route('/books')
+def books():
+    return render_template('site/books.html')
+
+@app.route('/us')
+def us():
+    return render_template('site/us.html')
+
+@app.route('/admin')
+def admin_index():
+    return render_template('admin/index.html')
+
+@app.route('/admin/login')
+def admin_login():
+    return render_template('admin/login.html')
+
 if __name__ =='__main__':
     app.run(debug=True)
