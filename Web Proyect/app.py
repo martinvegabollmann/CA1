5afae5113ebe2bf8d95d11cd351b2d0fd9c4e44e
martinvegabollmann <martinvegabollmann>
2023-07-12 02:58:53 +0100

    sixth updated

--- Web Proyect/app.py
+++ Web Proyect/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, session
 from flaskext.mysql import MySQL
 from datetime import datetime
 from flask import send_from_directory
 import os
 
+
 app=Flask(__name__)
 mysql=MySQL()
+app.secret_key="develoteca"
 
 app.config['MYSQL_DATABASE_HOST']='localhost'
 app.config['MYSQL_DATABASE_USER']='root'
@@ -40,15 +42,41 @@ def us():
 
 @app.route('/admin/')
 def admin_index():
+    if not 'login' in session:
+        return redirect("/admin/login")
     return render_template('admin/index.html')
 
 @app.route('/admin/login')
 def admin_login():
     return render_template('admin/login.html')
 
+@app.route('/admin/login', methods=['POST'])
+def admin_login_post():
+    _user=request.form['txtUser']
+    _password=request.form['txtPassword']
+    print(_user)
+    print(_password)
+
+    if _user=="admin" and _password=="1234":
+        session["login"]=True
+        session["user"]="administrator"
+        return redirect("/admin")
+
+    return render_template('admin/login.html')
+
+
+@app.route('/admin/close')
+def admin_login_close():
+    session.clear()
+    return redirect('/admin/login')
+
+
 @app.route('/admin/books')
 def admin_books():
 
+    if not 'login' in session:
+        return redirect("/admin/login")
+    
     connection=mysql.connect()
     cursor= connection.cursor()
     cursor.execute("SELECT * FROM `books`")
@@ -60,6 +88,10 @@ def admin_books():
 
 @app.route('/admin/books/save', methods=['POST'])
 def admin_books_save():
+
+    if not 'login' in session:
+        return redirect("/admin/login")
+     
     _name=request.form['txtName']
     _url=request.form['txtURL']
     _archive=request.files['txtImage']
@@ -88,6 +120,10 @@ def admin_books_save():
 
 @app.route('/admin/books/delete', methods=['POST'])
 def admin_books_delete():
+
+    if not 'login' in session:
+        return redirect("/admin/login")
+
     _id=request.form['txtID']
     print(_id)
 
